4ca42925618895884985a8d7bf9a72647fe7eafb
Luuk Visser <l.visser@duxgroup.nl>
2025-12-01 12:26:49 +0100

    day 1 solutions

--- solutions/1.py
+++ solutions/1.py
@@ -6,16 +6,26 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    import marimo as mo
+    from pathlib import Path
 
-    return (mo,)
+    import marimo as mo
+    import numpy as np
+
+    from advent_of_code_2025.utils import obtain_input
+    return Path, mo, np, obtain_input
+
+
+@app.cell
+def _(Path, obtain_input):
+    day_number = Path(__file__).stem
+    input_fp = obtain_input(day_number)
+    return day_number, input_fp
 
 
 @app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    from pathlib import Path
-    mo.md(f"# Day {Path(__file__).stem}")
+def _(day_number, mo):
+    mo.md(f"""
+    # Day {day_number}
     """)
     return
 
@@ -29,9 +39,24 @@ def _(mo):
 
 
 @app.cell
-def _():
-    # from advent_of_code_2025.grid import GridPoint, Direction, rotate_right
-    return
+def _(input_fp, np):
+    def instruction_to_turns(instruction):
+        direction = 1 if instruction[0] == "R" else -1
+        value = int(instruction[1:])
+        return direction * value
+
+
+    def get_positions(instructions, starting_pos=50):
+        return np.cumsum(
+            np.array([starting_pos] + [instruction_to_turns(instruction) for instruction in instructions])
+        )
+
+
+    instructions = open(input_fp).read().splitlines()
+    positions = get_positions(instructions)
+
+    (positions % 100 == 0).sum()
+    return (positions,)
 
 
 @app.cell(hide_code=True)
@@ -39,6 +64,18 @@ def _(mo):
     mo.md(r"""
     ## Part 2
     """)
+    return
+
+
+@app.cell
+def _(positions):
+    count = 0
+    for prev_pos, curr_pos in zip(positions, positions[1:]):
+        if prev_pos > curr_pos:
+            count += abs((curr_pos - 1) // 100 - (prev_pos - 1) // 100)
+        else:
+            count += abs(curr_pos // 100 - prev_pos // 100)
+    count
     return
 
 
